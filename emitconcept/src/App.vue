727b7a5f3006729d152085c7b2da18c844397964
aziya-500apps <aziya.shaik@500apps.com>
2022-04-08 10:54:21 +0530

    commit

---
+++ emitconcept/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <forms/>
+  </div>
+</template>
+
+<script>
+import forms from '../src/components/emit.vue'
+export default {
+  name: 'App',
+  components: {
+    forms
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
